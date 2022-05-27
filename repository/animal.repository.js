0c0ce209756cdb75e769983a1ed7f8b32843ed99
Pablo <pablogerheim@gmail.com>
2022-05-27 17:33:00 -0300

    first commit

--- repository/animal.repository.js
+++ repository/animal.repository.js
@@ -3,10 +3,26 @@ import { connect } from "./connect.js";
 
 async function create({ name, telefone }) {
     const conn = await connect();
-    const sql = 'INSERT INTO animal (name, telefone) VALUES ($1, $2)'
+    const sql = 'INSERT INTO animal (name, telefone) VALUES ($1, $2) RETURNING *'
     const values = [name, telefone]
     const res = await conn.query(sql, values)
     return {}
+}
+
+async function pirnt(params) {
+    const conn = await connect()
+    try {
+        if (params = "id") {
+
+        }
+
+        const res = await conn.query("SELECT * FROM animal")
+    } catch (error) {
+        throw err
+    } finally {
+        conn.release()
+    }
+
 }
 
 
